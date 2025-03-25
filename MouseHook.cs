fc828ac165cedafa1fcf118e2f296fd408a759bb
feralshining <racc00ns@naver.com>
2025-03-25 16:40:20 +0900

    Update MouseHook.cs

--- MouseHook.cs
+++ MouseHook.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.InteropServices;
@@ -10,13 +10,11 @@ namespace GTA_DIAMOND_MACRO
 {
     class MouseHook
     {
-
         public event MouseEventHandler OnMouseActivity;
 
         [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall, SetLastError = true)]
         private static extern int SetWindowsHookEx(int idHook, HookProc lpfn, IntPtr hMod, int dwThreadId);
 
-        //调用此函数卸载钩子
         [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
         public static extern bool UnhookWindowsHookEx(int idHook);
 
@@ -26,28 +24,18 @@ namespace GTA_DIAMOND_MACRO
         [DllImport("user32")]
         private static extern short GetKeyState(int vKey);
 
-        // 取得当前线程编号（线程钩子需要用到）
         [DllImport("kernel32.dll")]
         static extern int GetCurrentThreadId();
 
-        //使用WINDOWS API函数代替获取当前实例的函数,防止钩子失效
         [DllImport("kernel32.dll")]
         public static extern IntPtr GetModuleHandle(string name);
 
-        HookProc MouseHookProcedure; //声明KeyboardHookProcedure作为HookProc类型
+        HookProc MouseHookProcedure;
 
         static int hMouseHook = 0;
         public delegate int HookProc(int nCode, Int32 wParam, IntPtr lParam);
 
-        /// <summary>  
-        /// Windows NT/2000/XP: Installs a hook procedure that monitors low-level mouse input events.  
-        /// </summary>  
         private const int WH_MOUSE_LL = 14;
-        /// <summary>  
-        /// Windows NT/2000/XP: Installs a hook procedure that monitors low-level Mouse input events.  
-        /// </summary>  
-
-        //在安装全局钩子的时候，我们就要做替换了，将WH_MOUSE和WH_KEYBORAD分别换成WH_MOUSE_LL和WH_KEYBORAD_LL：  
 
         [StructLayout(LayoutKind.Sequential)]
         private class POINT
@@ -82,41 +70,19 @@ namespace GTA_DIAMOND_MACRO
         private const int WM_XButtonDown = 0x020B;
         private const int WM_XButtonUP = 0x020C;
 
-
-        //install hook 
         public void Start()
         {
-            // 安装鼠标钩子
             if (hMouseHook == 0)
             {
                 MouseHookProcedure = new HookProc(MouseHookProc);
                 hMouseHook = SetWindowsHookEx(WH_MOUSE_LL, MouseHookProcedure, GetModuleHandle(System.Diagnostics.Process.GetCurrentProcess().MainModule.ModuleName), 0);
-                //hKeyboardHook = SetWindowsHookEx(WH_KEYBOARD_LL, KeyboardHookProcedure, Marshal.GetHINSTANCE(Assembly.GetExecutingAssembly().GetModules()[0]), 0);
-                //************************************
-                //鼠标线程钩子
-                SetWindowsHookEx(13, MouseHookProcedure, IntPtr.Zero, GetCurrentThreadId());//指定要监听的线程idGetCurrentThreadId(),
-                //鼠标全局钩子,需要引用空间(using System.Reflection;)
-                //SetWindowsHookEx( 13,MouseHookProcedure,Marshal.GetHINSTANCE(Assembly.GetExecutingAssembly().GetModules()[0]),0);
-                //
-                //关于SetWindowsHookEx (int idHook, HookProc lpfn, IntPtr hInstance, int threadId)函数将钩子加入到钩子链表中，说明一下四个参数：
-                //idHook 钩子类型，即确定钩子监听何种消息，上面的代码中设为2，即监听鼠标消息并且是线程钩子，如果是全局钩子监听鼠标消息应设为13，
-                //线程钩子监听鼠标消息设为7，全局钩子监听鼠标消息设为14。lpfn 钩子子程的地址指针。如果dwThreadId参数为0 或是一个由别的进程创建的
-                //线程的标识，lpfn必须指向DLL中的钩子子程。 除此以外，lpfn可以指向当前进程的一段钩子子程代码。钩子函数的入口地址，当钩子钩到任何
-                //消息后便调用这个函数。hInstance应用程序实例的句柄。标识包含lpfn所指的子程的DLL。如果threadId 标识当前进程创建的一个线程，而且子
-                //程代码位于当前进程，hInstance必须为NULL。可以很简单的设定其为本应用程序的实例句柄。threaded 与安装的钩子子程相关联的线程的标识符
-                //如果为0，钩子子程与所有的线程关联，即为全局钩子
-                //************************************
-                //如果SetWindowsHookEx失败
+                SetWindowsHookEx(13, MouseHookProcedure, IntPtr.Zero, GetCurrentThreadId());
                 if (hMouseHook == 0)
                 {
-                    //Stop();
-                    throw new Exception("安装鼠标钩子失败");
+                    throw new Exception("마우스 후크 설치 실패");
                 }
             }
         }
-
-        //下面是关于鼠标和鼠标的两个Callback函数：  
-
 
         private int MouseHookProc(int nCode, int wParam, IntPtr lParam)
         {
@@ -136,7 +102,6 @@ namespace GTA_DIAMOND_MACRO
                         button = MouseButtons.Right;
                         break;
                     case WM_XButtonDown:
-
                         if (mouseHookStruct.mouseData >> 16 == 1)
                         {
                             button = MouseButtons.XButton1;
@@ -145,8 +110,6 @@ namespace GTA_DIAMOND_MACRO
                         {
                             button = MouseButtons.XButton2;
                         }
-
-
                         break;
                     case WM_MOUSEWHEEL:
                         mouseDelta = (short)((mouseHookStruct.mouseData >> 16) & 0xffff);
@@ -176,15 +139,13 @@ namespace GTA_DIAMOND_MACRO
         {
             bool retMouse = true;
 
-
             if (hMouseHook != 0)
             {
                 retMouse = UnhookWindowsHookEx(hMouseHook);
                 hMouseHook = 0;
             }
 
-            if (!(retMouse)) throw new Exception("卸载钩子失败！");
+            if (!(retMouse)) throw new Exception("후크 제거 실패!");
         }
-
     }
 }
